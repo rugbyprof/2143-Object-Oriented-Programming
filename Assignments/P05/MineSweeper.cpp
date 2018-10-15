992359c1bd849df5a25d869583b27e23befec2f2
Terry Griffin <rugbyprof@users.noreply.github.com>
2018-10-15 11:49:58 -0500

    Update MineSweeper.cpp

--- Assignments/P05/MineSweeper.cpp
+++ Assignments/P05/MineSweeper.cpp
@@ -3,11 +3,29 @@
 #include <ctime>
 #include <chrono>
 
+#define WIDTH 600
+#define HEIGHT 600
+#define CELLWIDTH 24
+#define CELLHEIGHT 24
+
 struct MineSquare {
 	sf::Sprite sprite;
 	int bombNeighbors;
 	int covered;
 };
+
+struct CellPosition {
+	int r;
+	int c;
+};
+
+CellPosition getMineCell(int x,int y) {
+	CellPosition p;
+	 p.c = x / CELLWIDTH;
+	 p.r = y / CELLHEIGHT;
+
+	 return p;
+}
 
 int main()
 {
@@ -18,10 +36,18 @@ int main()
 
 
 	sf::Texture squareTexture;
-	if (!squareTexture.loadFromFile("minesweeper/square.png"))
+	if (!squareTexture.loadFromFile("square.png"))
 	{
 		std::cout << "Could not load square" << std::endl;
 	}
+
+
+	sf::Texture flagTexture;
+	if (!flagTexture.loadFromFile("flag.png"))
+	{
+		std::cout << "Could not load flag" << std::endl;
+	}
+
 
 	MineSquare** MineWorld;
 
@@ -35,18 +61,18 @@ int main()
 		for (int j = 0; j < 25; j++) {
 			MineWorld[i][j].sprite.setTexture(squareTexture);
 			MineWorld[i][j].sprite.setScale(sf::Vector2f(0.1f, 0.1f));
-			MineWorld[i][j].sprite.setPosition(sf::Vector2f(i*24, j*24));
+			MineWorld[i][j].sprite.setPosition(sf::Vector2f(i * 24, j * 24));
 		}
 	}
 
 	// create the window
-	sf::RenderWindow window(sf::VideoMode(600, 600), "My window");
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My window");
 
 
-	sf::Sprite sprite;
-	sprite.setTexture(squareTexture);
-	sprite.setScale(sf::Vector2f(0.1f, 0.1f));
-	sprite.setPosition(sf::Vector2f(10, 50));
+	//sf::Sprite sprite;
+	//sprite.setTexture(squareTexture);
+	//sprite.setScale(sf::Vector2f(0.1f, 0.1f));
+	//sprite.setPosition(sf::Vector2f(10, 50));
 
 	sf::Vector2i localPosition;
 
@@ -79,28 +105,26 @@ int main()
 			if (!mouseWasPressed) {
 				t1 = std::chrono::high_resolution_clock::now();
 				std::cout << localPosition.x << "," << localPosition.y << std::endl;
+				CellPosition p = getMineCell(localPosition.x, localPosition.y);
+				std::cout << p.c << "," << p.r << std::endl;
+				MineWorld[p.c][p.r].sprite.setTexture(flagTexture);
 				mouseWasPressed = true;
-				
 			}
-			
+
 		}
-		
+
 		for (int i = 0; i < 25; i++) {
 			for (int j = 0; j < 25; j++) {
-				MineWorld[i][j].sprite.setTexture(squareTexture);
-				MineWorld[i][j].sprite.setScale(sf::Vector2f(0.1f, 0.1f));
-				MineWorld[i][j].sprite.setPosition(sf::Vector2f(i * 24, j * 24));
 				window.draw(MineWorld[i][j].sprite);
 			}
 		}
 
 		t2 = std::chrono::high_resolution_clock::now();
 		time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-		if (time_span.count() > 0.5 ) {
+		if (time_span.count() > 0.5) {
 			mouseWasPressed = false;
 		}
 
-		
 		// end the current frame
 		window.display();
 	}
