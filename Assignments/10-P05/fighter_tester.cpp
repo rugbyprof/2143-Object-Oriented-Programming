4acab6ad3e04637372d57468d19e8d379e10234a
Terry Griffin <rugbyprof@users.noreply.github.com>
2022-11-14 11:34:30 -0600

    Update fighter_tester.cpp

--- Assignments/10-P05/fighter_tester.cpp
+++ Assignments/10-P05/fighter_tester.cpp
@@ -9,11 +9,20 @@ using namespace std;
 int main() {
     srand(time(0));
 
-    BaseFighter f1;
+    BaseFighter *f1 = new BaseFigher;
+    
+    vector<BaseFighter*> fighters;
+    
+    
+    # Optionally create a vector of 100 BaseFighters
+    for(int i=0;i<100;i++){
+        fighters.push_back(new BaseFighter);
+    }
+    
+    
+    cout<<*f1<<endl;
 
-    cout<<f1<<endl;
-
-    cout<<f1.attack()<<endl;
+    cout<<f1->attack()<<endl;
 
     return 0;
 }
